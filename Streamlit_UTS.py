856d248660b7e8883944b06940be8baa11fb9d41
RexTazy <147713337+RexTazy@users.noreply.github.com>
2025-04-21 16:55:32 +0700

    Update Streamlit_UTS.py

--- Streamlit_UTS.py
+++ Streamlit_UTS.py
@@ -14,24 +14,72 @@ st.title("Fikri Aziz Biruni Project - 2702356362")
 st.title("Hotel Booking Cancellation Prediction")
 st.markdown("Masukkan data pemesanan hotel untuk memprediksi apakah pemesanan akan dibatalkan atau tidak.")
 
+def fill_test_case(case_id):
+    if case_id == 1:
+        st.session_state.no_of_adults = 2
+        st.session_state.no_of_children = 1
+        st.session_state.no_of_weekend_nights = 1
+        st.session_state.no_of_week_nights = 3
+        st.session_state.type_of_meal_plan = 'Meal Plan 1'
+        st.session_state.required_car_parking_space = 1
+        st.session_state.room_type_reserved = 'Room Type 1'
+        st.session_state.lead_time = 100
+        st.session_state.arrival_year = 2017
+        st.session_state.arrival_month = 7
+        st.session_state.arrival_date = 15
+        st.session_state.market_segment_type = 'Online'
+        st.session_state.repeated_guest = 0
+        st.session_state.no_of_previous_cancellations = 0
+        st.session_state.no_of_previous_bookings_not_canceled = 2
+        st.session_state.avg_price_per_room = 80.5
+        st.session_state.no_of_special_requests = 1
+
+    elif case_id == 2:
+        st.session_state.no_of_adults = 1
+        st.session_state.no_of_children = 0
+        st.session_state.no_of_weekend_nights = 0
+        st.session_state.no_of_week_nights = 1
+        st.session_state.type_of_meal_plan = 'Not Selected'
+        st.session_state.required_car_parking_space = 0
+        st.session_state.room_type_reserved = 'Room Type 3'
+        st.session_state.lead_time = 10
+        st.session_state.arrival_year = 2018
+        st.session_state.arrival_month = 12
+        st.session_state.arrival_date = 5
+        st.session_state.market_segment_type = 'Corporate'
+        st.session_state.repeated_guest = 1
+        st.session_state.no_of_previous_cancellations = 2
+        st.session_state.no_of_previous_bookings_not_canceled = 1
+        st.session_state.avg_price_per_room = 150.0
+        st.session_state.no_of_special_requests = 0
+
+# Tombol test case
+col1, col2 = st.columns(2)
+with col1:
+    if st.button("Test Case 1"):
+        fill_test_case(1)
+with col2:
+    if st.button("Test Case 2"):
+        fill_test_case(2)
+
 # ===== Input Form dari User =====
-no_of_adults = st.number_input('Number of Adults', min_value=0, max_value=4)
-no_of_children = st.number_input('Number of Children', min_value=0,  max_value=10)
-no_of_weekend_nights = st.number_input('Number of Weekend Nights', min_value=0, max_value=7)
-no_of_week_nights = st.number_input('Number of Week Nights', min_value=0, max_value=17)
-type_of_meal_plan = st.selectbox('Meal Plan', ['Meal Plan 1', 'Meal Plan 2', 'Meal Plan 3', 'Not Selected'])
-required_car_parking_space = st.selectbox('Required Car Parking Space', [0, 1])
-room_type_reserved = st.selectbox('Room Type Reserved', ['Room Type 1', 'Room Type 2', 'Room Type 3', 'Room Type 4', 'Room Type 5', 'Room Type 6', 'Room Type 7'])
-lead_time = st.number_input('Lead Time', min_value=0, max_value=443)
-arrival_year = st.number_input('Arrival Year', min_value=2017, max_value=2018, value=2017)
-arrival_month = st.number_input('Arrival Month', min_value=1, max_value=12, value=3)
-arrival_date = st.number_input('Arrival Date', min_value=1, max_value=31, value=28)
-market_segment_type = st.selectbox('Market Segment Type', ['Aviation', 'Complementary', 'Corporate', 'Offline', 'Online'])
-repeated_guest = st.number_input('Repeated Guest', min_value=0, max_value=1, value=0)
-no_of_previous_cancellations = st.number_input('Number of Previous Cancellations', min_value=0, max_value=13)
-no_of_previous_bookings_not_canceled = st.number_input('Number of Previous Bookings Not Canceled', min_value=0, max_value=58)
-avg_price_per_room = st.number_input('Average Price per Room', min_value=0.0, max_value=540.00)
-no_of_special_requests = st.number_input('Number of Special Requests', min_value=0, max_value=5)
+no_of_adults = st.number_input('Number of Adults', min_value=0, max_value=4, key='no_of_adults')
+no_of_children = st.number_input('Number of Children', min_value=0,  max_value=10, key='no_of_children')
+no_of_weekend_nights = st.number_input('Number of Weekend Nights', min_value=0, max_value=7, key='no_of_weekend_nights')
+no_of_week_nights = st.number_input('Number of Week Nights', min_value=0, max_value=17, key='no_of_week_nights')
+type_of_meal_plan = st.selectbox('Meal Plan', ['Meal Plan 1', 'Meal Plan 2', 'Meal Plan 3', 'Not Selected'], key='type_of_meal_plan')
+required_car_parking_space = st.selectbox('Required Car Parking Space', [0, 1], key='required_car_parking_space')
+room_type_reserved = st.selectbox('Room Type Reserved', ['Room Type 1', 'Room Type 2', 'Room Type 3', 'Room Type 4', 'Room Type 5', 'Room Type 6', 'Room Type 7'], key='room_type_reserved')
+lead_time = st.number_input('Lead Time', min_value=0, max_value=443, value=100, key='lead_time')
+arrival_year = st.number_input('Arrival Year', min_value=2017, max_value=2018, value=2017, key='arrival_year')
+arrival_month = st.number_input('Arrival Month', min_value=1, max_value=12, value=3, key='arrival_month')
+arrival_date = st.number_input('Arrival Date', min_value=1, max_value=31, value=28, key='arrival_date')
+market_segment_type = st.selectbox('Market Segment Type', ['Aviation', 'Complementary', 'Corporate', 'Offline', 'Online'], key='market_segment_type')
+repeated_guest = st.number_input('Repeated Guest', min_value=0, max_value=1, value=0, key='repeated_guest')
+no_of_previous_cancellations = st.number_input('Number of Previous Cancellations', min_value=0, max_value=13, value=0, key='no_of_previous_cancellations')
+no_of_previous_bookings_not_canceled = st.number_input('Number of Previous Bookings Not Canceled', min_value=0, max_value=58, value=0, key='no_of_previous_bookings_not_canceled')
+avg_price_per_room = st.number_input('Average Price per Room', min_value=0.0, max_value=540.00, key='avg_price_per_room')
+no_of_special_requests = st.number_input('Number of Special Requests', min_value=0, max_value=5, key='no_of_special_requests')
 
 # ===== Submit Button =====
 if st.button("Predict"):
